c88cf292f7205b26753dd900ba9a94976d1cbfd2
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2023-11-12 15:55:19 +0000

    make the navigation do things

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
+
+int currentPageIndex = 1;
+
+const navigationDestinations = [
+    NavigationDestination(icon: Icon(Icons.abc), label: 'Load Module'),
+    NavigationDestination(icon: Icon(Icons.accessibility), label: 'Quick Calculator'),
+    NavigationDestination(icon: Icon(Icons.info_outline), label: 'About'),
+  ];
+
+// function which detmines based on a number which page you want to be sent to (fn called when tap on icon on nav bar returns index from 0 to 2. we have to keep tabs on which page wants to be opened ourselves). return string, name of the page we are trying to go to (path to the file in the webserver)
+// also need a var to store page which is open - f u says flutter we have to keep tabs on this
+
+String getNewPageIndexStr(int newPageIndexInt){
+  currentPageIndex = newPageIndexInt;
+  switch(newPageIndexInt){
+    case 0:
+      return "/load-module";
+    case 2:
+      return "/about";
+    case 1:
+    default:
+      return "/";
+    
+  }
+}
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,113 +40,146 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const QuickCalculator(title: 'Flutter Demo Home Page'),
+      routes: <String, WidgetBuilder> {
+        "/load-module":(context) => const LoadModule(title: 'Load Module'),
+        "/about":(context) => const About(title: 'About')
+        }
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class QuickCalculator extends StatefulWidget {
+  const QuickCalculator({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<QuickCalculator> createState() => _QuickCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _QuickCalculatorState extends State<QuickCalculator> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const Center(
+        
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+              'Quick Calculator',
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: NavigationBar(
+        destinations: navigationDestinations,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          if(index != currentPageIndex){
+            Navigator.pushReplacementNamed(context, getNewPageIndexStr(index));
+          }
+        },
+      ),
+    );
+  }
+}
+
+class LoadModule extends StatefulWidget {
+  const LoadModule({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LoadModule> createState() => _LoadModuleState();
+}
+
+class _LoadModuleState extends State<LoadModule> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Load Module',
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: navigationDestinations,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          if(index != currentPageIndex){
+            Navigator.pushReplacementNamed(context, getNewPageIndexStr(index));
+          }
+        },
+      ),
+    );
+  }
+}
+
+class About extends StatefulWidget {
+  const About({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'About',
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: navigationDestinations,
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          if(index != currentPageIndex){
+            Navigator.pushReplacementNamed(context, getNewPageIndexStr(index));
+          }
+        },
+      ),
     );
   }
 }
